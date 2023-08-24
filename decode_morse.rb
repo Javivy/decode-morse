6fd57d9c075593790f58bb3584c5ad71a5637419
Javier Vargas <Javieralejandroortga@gmail.com>
2023-08-24 12:39:08 -0400

    Create decode_word function

---
+++ decode_morse.rb
@@ -0,0 +1,46 @@
+LETTERS = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..'
+}.freeze
+
+def decode_char(ltr)
+  letter = ''
+  LETTERS.each do |key, value|
+    letter += key.to_s if ltr == value
+  end
+  letter
+end
+
+def decode_word(word)
+  decoded_word = ''
+  new_word = word.split
+
+  new_word.each do |char|
+    decoded_word += decode_char(char)
+  end
+  decoded_word
+end
